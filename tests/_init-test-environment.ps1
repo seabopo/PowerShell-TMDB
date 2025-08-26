8a7bd46703bd6df4cdd21b7efe41ce7860b1b525
seabopo <seabopo@icloud.com>
2025-08-26 00:19:35 -0500

    Move test function init code to a shared file.

---
+++ tests/_init-test-environment.ps1
@@ -0,0 +1,30 @@
+#==================================================================================================================
+#==================================================================================================================
+# Initialize Test Environment
+#==================================================================================================================
+#==================================================================================================================
+
+Clear-Host
+
+$ErrorActionPreference = "Stop"
+
+$env:PS_STATUSMESSAGE_VERBOSE_MESSAGE_TYPES = '["Process","Information","Debug","FunctionCall","FunctionResult"]'
+$env:PS_STATUSMESSAGE_SHOW_VERBOSE_MESSAGES = $true
+
+Set-Location  -Path $PSScriptRoot
+Push-Location -Path $PSScriptRoot
+
+if (((Get-Location).Path) -match 'PowerShell-[^/\\]*') {
+    $repoName         = $Matches[0]
+    $repoPath         = ((Get-Location).Path -Replace $('{0}.*' -f $repoName),$repoName)
+    $reposPath        = ((Get-Location).Path -Replace $('{0}.*' -f $repoName),'')
+    $modulePath       = Join-Path -Path $repoPath -ChildPath $($repoName.Replace('PowerShell-','po.'))
+    $mediaClassesPath = Join-Path -Path $reposPath -ChildPath 'PowerShell-MediaClasses/po.MediaClasses'
+}
+else {
+    Write-Host 'Unexpected repo path found. Script execution halted.' -ForegroundColor Red
+    exit
+}
+
+Import-Module $mediaClassesPath
+Import-Module $modulePath -Force
