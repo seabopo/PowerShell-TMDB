0583a0964e6ccc311e74d57a13c27a7c8d36ae41
seabopo <seabopo@icloud.com>
2025-08-27 23:38:28 -0500

    Add season functions.

---
+++ po.TMDB/private/Get-TVSeasonFromDetails.ps1
@@ -0,0 +1,71 @@
+Function Get-TVSeasonFromDetails {
+    <#
+    .DESCRIPTION
+        Creates an object which contains information about a single TV Season, including the episodes.
+
+    .OUTPUTS
+        A single [TVSeason] object with the following properties:
+            - [String]      Source
+            - [String]      ID
+            - [String]      ShowID
+            - [int32]       Number
+            - [String]      Name
+            - [String]      Description
+            - [Credit[]]    Cast
+            - [Credit[]]    Crew
+            - [String]      Network
+            - [String]      Year
+            - [String]      FirstAirDate
+            - [String]      LastAirDate
+            - [int32]       TotalEpisodes
+            - [String]      PosterPath
+            - [String]      BackdropPath
+            - [String]      PosterURL
+            - [String]      BackdropURL
+            - [TVEpisode[]] Episodes
+            - [Item[]]      ExternalIDs
+            - [Image[]]     Images
+
+    .PARAMETER SeasonData
+        REQUIRED. Hashtable. Alias: -d. Season data.
+
+    .EXAMPLE
+        Get-TVSeasonFromDetails  -d @{ ...}
+    
+    .EXAMPLE
+        @{ ...} | Get-TVSeasonFromDetails 
+
+    #>
+    [OutputType([TVSeason])]
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)] [Alias('d')] [PSCustomObject] $SeasonData
+    )
+
+    process {
+        
+        Write-Msg -FunctionCall -IncludeParameters
+        
+        $season = $([TVSeason]::New(@{
+            Source           = 'TMDB'
+            ID               = $SeasonData.id
+            Number           = $SeasonData.season_number
+            Name             = $SeasonData.name
+            Description      = $SeasonData.overview
+            FirstAirDate     = $SeasonData.air_date
+            TotalEpisodes    = $SeasonData.episode_count
+            PosterPath       = $SeasonData.poster_path
+            PosterURL        = @( if ( [String]::IsNullOrEmpty($SeasonData.poster_path) ) { $null }
+                                  else { $IMG_BASE_URI + $SeasonData.poster_path } )
+            Year             = $( if ([String]::IsNullOrEmpty($SeasonData.air_date)) { '' } 
+                                  else { ([datetime]($SeasonData.air_date)).Year } )
+            Episodes         = $( if ([String]::IsNullOrEmpty($SeasonData.episodes)) { $null } 
+                                  else { $SeasonData.episodes | Get-TVEpisodeFromDetails } )
+
+        }))
+
+        Write-Msg -FunctionResult -o $season
+
+        return $season
+    }
+}
---
+++ po.TMDB/public/Get-TMdbTVSeason.ps1
@@ -0,0 +1,215 @@
+Function Get-TMdbTVSeason {
+    <#
+    .DESCRIPTION
+        Gets the details of a single TV Season, which includes the data for all episodes.
+
+    .OUTPUTS
+        A single [TVSeason] object with the following properties:
+            - [String]      Source
+            - [String]      ID
+            - [String]      ShowID
+            - [int32]       Number
+            - [String]      Name
+            - [String]      Description
+            - [Credit[]]    Cast
+            - [Credit[]]    Crew
+            - [String]      Network
+            - [String]      Year
+            - [String]      FirstAirDate
+            - [String]      LastAirDate
+            - [int32]       TotalEpisodes
+            - [String]      PosterPath
+            - [String]      BackdropPath
+            - [String]      PosterURL
+            - [String]      BackdropURL
+            - [TVEpisode[]] Episodes
+            - [Item[]]      ExternalIDs
+            - [Image[]]     Images
+
+    .PARAMETER SeriesID
+        REQUIRED. String. Alias: -i, -ShowID. The TV Series/Show ID. Example: 615
+
+    .PARAMETER SeasonNumber
+        REQUIRED. Int. Alias: -s. The Season Number of the Series/Show. Example: 1
+
+    .PARAMETER IncludeSeasonCastCredits
+        OPTIONAL. Switch. Alias: -ccs. The TMDB Season details do not include the credits for cast or crew 
+        members, which is a separate TMDB query. Use this switch to include the cast and crew credits 
+        in the season details.
+
+    .PARAMETER IncludeSeasonImages
+        OPTIONAL. Switch. Alias: -is. TMDB includes a poster image representing the season in the season 
+        details. However, seasons may have multiple images available to select from. To include links to all of
+        the images include this parameter.
+
+    .PARAMETER IncludeSeasonExternalIDs
+        OPTIONAL. Switch. Alias: -xs. TMDB includes the IDs used by other media databases (IMDB, Freebase, TVDB, 
+        TVRage and Wikidata) for many of the the entries in it's catalog. Use this switch to include the IDs 
+        of those entries (if they exist) in the season data.
+
+    .PARAMETER IncludeEpisodeCastCredits
+        OPTIONAL. Switch. Alias: -cce. The TMDB Episode details include the credits for crew members and guest 
+        stars but do not include the cast credits, which is a separate TMDB query. Use this switch to 
+        include the cast credits in the episode details.
+
+        Please note that this parameter is mutually exclusive with the IncludeSeasonCastCreditsForEpisodes 
+        parameter.
+
+    .PARAMETER IncludeEpisodeImages
+        OPTIONAL. Switch. Alias: -ie. TMDB includes a single still image representing the episode in the episode 
+        details. However, episodes may have multiple images available to select from. To include links to all of
+        the images include this parameter.
+
+    .PARAMETER IncludeEpisodeExternalIDs
+        OPTIONAL. Switch. Alias: -xe. TMDB includes the IDs used by other media databases (IMDB, Freebase, TVDB, 
+        TVRage and Wikidata) for many of the the entries in it's catalog. Use this switch to include the IDs 
+        of those entries (if they exist) in the episode data.
+
+    .PARAMETER IncludeSeasonCastCreditsForEpisodes
+        OPTIONAL. Switch. Alias: -escc. The TMDB Season details include the episode data. The episode data includes
+        crew and guest star credits, but does not include cast credits, which is a separate TMDB query. Use this 
+        switch if you want to apply the TV Series/Show season cast credits to each episode in that Season. 
+        
+        Please note that this parameter is mutually exclusive with the IncludeEpisodeCastCreditsForEpisodes 
+        parameter.
+
+    .PARAMETER Language
+        OPTIONAL. String. Alias: -l. The desired language. Default: The current PowerShell language/culture.
+
+    .EXAMPLE
+        Get-TMdbTVSeason -ShowID 615 -SeasonNumber 1
+
+    .EXAMPLE
+        Get-TMdbTVSeason -ShowID 615 -SeasonNumber 1 -IncludeSeasonCastCredits -IncludeSeasonImages
+
+    .EXAMPLE
+        Get-TMdbTVSeason -ShowID 615 -SeasonNumber 1 -IncludeEpisodeCastCredits -IncludeEpisodeImages
+
+    .EXAMPLE
+        Get-TMdbTVSeason -i 615 -s 1 -ccs -is -xs -ccse -ie -xe
+
+    #>
+    [OutputType([TVSeason])]
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)] [Alias('i','ShowID')] [String] $SeriesID,
+        [Parameter(Mandatory)] [Alias('s')]          [Int]    $SeasonNumber,
+        [Parameter()]          [Alias('ccs')]        [Switch] $IncludeSeasonCastCredits,
+        [Parameter()]          [Alias('is')]         [Switch] $IncludeSeasonImages,
+        [Parameter()]          [Alias('xs')]         [Switch] $IncludeSeasonExternalIDs,
+        [Parameter()]          [Alias('cce')]        [Switch] $IncludeEpisodeCastCredits,
+        [Parameter()]          [Alias('ie')]         [Switch] $IncludeEpisodeImages,
+        [Parameter()]          [Alias('xe')]         [Switch] $IncludeEpisodeExternalIDs,
+        [Parameter()]          [Alias('ccse')]       [Switch] $IncludeSeasonCastCreditsForEpisodes,
+        [Parameter()]          [Alias('l')]          [String] $Language = $((Get-Culture).Name.ToString())
+    )
+
+    process {
+
+        Write-Msg -FunctionCall -IncludeParameters
+
+        if ( $IncludeSeasonCastCreditsForEpisodes -and $IncludeEpisodeCastCredits ) {
+            Throw $('The IncludeSeasonCastCreditsForEpisodes and IncludeEpisodeCastCredits parameters
+                     are mutually exclusive. Please use only one of these parameters at a time.')
+        }
+
+        $SearchURL = @(
+            $( $API_BASE_URI ),
+            $( '/tv/{0}'       -f $SeriesID ),
+            $( '/season/{0}'   -f $SeasonNumber ),
+            $( '?language={0}' -f $Language )
+        ) -Join ''
+
+        Write-Msg -p -ps -m ( 'Querying TMDB for TV Season Details ...' )
+        Write-Msg -i -il 1 -m ( 'Series/Show ID: {0}'  -f $SeriesID )
+        Write-Msg -i -il 1 -m ( 'Season Number: {0}'   -f $SeasonNumber )
+        Write-Msg -d -il 1 -m ( 'Token: {0}...'        -f $($env:TMDB_API_TOKEN).Substring(0,8) )
+        Write-Msg -d -il 1 -m ( 'Query: {0}'           -f $SearchURL )
+
+        $r = Invoke-HttpRequest -u $SearchURL -t $env:TMDB_API_TOKEN -j -d
+
+        if ( $r.success -and $r.statusCode -eq 200 ) {
+            
+            $s = ($r.value | ConvertFrom-Json)
+
+            if ( -not ([String]::IsNullOrEmpty($s)) ) {
+
+                $season = $( $s | Get-TVSeasonFromDetails )
+
+                if ( [String]::IsNullOrEmpty($season.ShowID) ) {
+                    $season.ShowID = $SeriesID
+                }
+
+                if ( $IncludeSeasonCastCredits -or $IncludeSeasonCastCreditsForEpisodes ) {
+                    $c = Get-TMdbTVCredits -i $SeriesID -s $SeasonNumber
+                    if ( $c.success ) {
+                        if ( $IncludeSeasonCastCredits ) {
+                            $season.Cast = $c.value.cast
+                            $season.Crew = $c.value.crew
+                        }
+                        if ( $IncludeSeasonCastCreditsForEpisodes ) {
+                            $season.Episodes | ForEach-Object {
+                                if ( $c.success ) {
+                                    $_.Cast = $c.value.cast
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if ( $IncludeSeasonImages ) {
+                    $i = Get-TMdbTVImages -i $SeriesID -s $SeasonNumber
+                    if ( $i.success ) {
+                        $season.Images = $i.value
+                    }
+                }
+
+               if ( $IncludeSeasonExternalIDs ) {
+                    $x = Get-TMdbTVExternalIDs -i $SeriesID -s $SeasonNumber
+                    if ( $x.success ) {
+                        $season.ExternalIDs = $x.value
+                    }
+                }
+
+                if ( $IncludeEpisodeCastCredits ) {
+                    $season.Episodes | ForEach-Object {
+                        $c = Get-TMdbTVCredits -i $_.ShowID -s $_.Season -e $_.Number
+                        if ( $c.success ) {
+                            $_.Cast = $c.value.cast
+                        }
+                    }
+                }
+
+                if ( $IncludeEpisodeImages ) {
+                    $season.Episodes | ForEach-Object {
+                        $i = Get-TMdbTVImages -i $_.ShowID -s $_.Season -e $_.Number
+                        if ( $i.success ) {
+                            $_.Images = $i.value
+                        }
+                    }
+                }
+
+                if ( $IncludeEpisodeExternalIDs ) {
+                    $season.Episodes | ForEach-Object {
+                        $x = Get-TMdbTVExternalIDs -i $_.ShowID -s $_.Season -e $_.Number
+                        if ( $x.success ) {
+                            $_.ExternalIDs = $x.value
+                        }
+                    }
+                }
+
+            }
+
+        }
+        elseif ( -not $r.success -and $r.statusCode -eq 404 ) {
+            Write-Msg -e -ps -ds -m $('No results found for query.')
+        }
+        else {
+            Write-Msg -e -ps -ds -m $($r.message)
+        }
+
+        Write-Msg -FunctionResult -Object $season -MaxRecursionDepth 5
+
+        return @{ success = $r.success; value = $season }
+    }
+}
